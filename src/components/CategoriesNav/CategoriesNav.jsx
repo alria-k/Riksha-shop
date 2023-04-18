712595128f4d822b6c25eda00d14c429b78136b7
Alria <artur.kirovich@gmail.com>
2023-04-18 22:38:32 +0600

    finished changing category

--- src/components/CategoriesNav/CategoriesNav.jsx
+++ src/components/CategoriesNav/CategoriesNav.jsx
@@ -1,64 +1,75 @@
+import classNames from "classnames";
 import "./CategoriesNav.scss";
 
-export function CategoriesNav() {
+export function CategoriesNav({ swtch = false, handler, currCategory }) {
   const categoriesItems = [
     {
       id: 1,
       name: "Пицца",
+      key: "pizza",
       img: "./src/assets/img/categories/pizza.svg",
       alt: "Pizza",
     },
     {
       id: 2,
       name: "Суши",
+      key: "sushi",
       img: "./src/assets/img/categories/sushi.svg",
       alt: "Sushi",
     },
     {
       id: 3,
       name: "Роллы",
+      key: "rolls",
       img: "./src/assets/img/categories/rolls.svg",
       alt: "Rolle",
     },
     {
       id: 4,
       name: "Сеты",
+      key: "sets",
       img: "./src/assets/img/categories/set.svg",
       alt: "Sets",
     },
     {
       id: 5,
       name: "Wok",
+      key: "wok",
       img: "./src/assets/img/categories/wok.svg",
       alt: "Wok",
     },
     {
       id: 6,
       name: "Супы",
+      key: "soups",
       img: "./src/assets/img/categories/soup.svg",
       alt: "Soup",
     },
     {
       id: 7,
       name: "Салаты",
+      key: "salads",
       img: "./src/assets/img/categories/salads.svg",
       alt: "Salads",
     },
     {
       id: 8,
       name: "Десерты",
+      key: "deserts",
       img: "./src/assets/img/categories/dessert.svg",
       alt: "Desserts",
     },
     {
       id: 9,
       name: "Напитки",
+      key: "drinks",
       img: "./src/assets/img/categories/drink.svg",
       alt: "Drinks",
     },
     {
       id: 10,
       name: "Акции",
+      key: "sale",
       img: "./src/assets/img/categories/sale.svg",
       alt: "Sale",
     },
@@ -69,7 +80,15 @@ export function CategoriesNav() {
       {categoriesItems.map((elem) => {
         return (
           <li key={elem.id} className="categories-item">
-            <a className="link-text categories-link" href="#f">
+            <a
+              className={classNames({
+                "link-text": true,
+                "categories-link": true,
+                "categories-link--active": swtch && currCategory == elem.key,
+              })}
+              onClick={(e) => handler(e, elem.key)}
+              href="#f"
+            >
               <img className="categories-img" src={elem.img} alt={elem.alt} />
               {elem.name}
             </a>
--- src/components/Main/components/Categories/Categories.jsx
+++ src/components/Main/components/Categories/Categories.jsx
@@ -3,18 +3,31 @@ import { CategoriesNav, ItemCard } from "../../index";
 import "./Categories.scss";
 
 export function Categories({ obj }) {
-  const [category, setCategory] = React.useState("rolls");
+  const [category, setCategory] = React.useState("pizza");
+  const [currPage, setCurrPage] = React.useState(1);
+  const amoutOfItems = 15;
+  let pages;
 
   const items = obj.items.read();
   const sale = obj.sale.read();
 
+  function handlerCategory(event, targetCategory) {
+    event.preventDefault();
+    setCategory(targetCategory);
+  }
+
   return (
     <div className="categories__container">
       <h1 className="blocks__title">Категории</h1>
-      <CategoriesNav />
+      <CategoriesNav
+        swtch={true}
+        handler={handlerCategory}
+        currCategory={category}
+      />
       <div className="categories__list">
-        {items.map((elem) => {
-          return [...Array(20).keys()].map((e, index) => {
+        {items.map((elem, index) => {
+          pages = Math.floor(elem[category].items.length / amoutOfItems);
+          return [...Array(amoutOfItems * currPage).keys()].map((e, index) => {
             if (index < elem[category].items.length) {
               return (
                 <ItemCard
@@ -27,6 +40,7 @@ export function Categories({ obj }) {
           });
         })}
       </div>
+      {pages ? <div>lol</div> : null}
     </div>
   );
 }
