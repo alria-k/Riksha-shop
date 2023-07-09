62cd6c9ae9341f0bf112f518acda0bdc0bd69dfa
Alria <artur.kirovich@gmail.com>
2023-07-09 23:14:51 +0600

    mixed sale and items data

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,17 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        
         <Route path="/about" element={<MainPage />} />
+        
         <Route path="/delivery-payment" element={<MainPage />} />
+        
         <Route path="/contacts" element={<MainPage />} />
+        
         <Route path="/bonuses" element={<MainPage />} />
+        
         <Route path="/vacancy" element={<MainPage />} />
+
         <Route path="/:category" element={<Catalog />} />
         <Route path="/:category/:item" element={<ItemPage />} />
       </Routes>
